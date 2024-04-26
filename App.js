4d5120b0f06c1a2484b53c8c163222cbfadeb956
Shivam Ramdhani <ramdhanishivam7678@gmail.com>
2024-04-26 14:13:12 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    'div',
+    {id: "parent"},
+    React.createElement(
+        'div',
+        {id: "child"}, 
+        [React.createElement('h1',
+         {}, 
+         'Hello, from React!'), React.createElement('h1',
+         {}, 
+         'Hello, from React!')]));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
