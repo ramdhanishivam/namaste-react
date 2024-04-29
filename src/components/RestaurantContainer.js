e0dcfe8bf2beb5654d8ed1e5ab112bf577ae3f2c
Shivam Ramdhani <ramdhanishivam7678@gmail.com>
2024-04-29 11:18:14 +0530

    swiggy api connected and shimmer effect implemented

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,16 +1,51 @@
 import React from "react";
 import { restaurantDataFromSwiggy } from "./utils/restaurantDataFromSwiggy";
 import { RestaurantCard } from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // RestaurantContainer component
 export const RestaurantContainer = () => {
-    const [restaurantData, setRestaurantData] = useState(restaurantDataFromSwiggy);
+    const [restaurantData, setRestaurantData] = useState([]);
+    useEffect(() => {
+        console.log("RestaurantContainer component mounted");
+        fetchSwiggyData();
+    }, []);
+
+    const fetchSwiggyData = async () => {
+        try {
+            const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.30080&lng=73.20430&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const data = await response.json();
+            setRestaurantData(data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        } catch (error) {
+            console.error("Error fetching data from Swiggy API", error);
+        }
+    };
+    
+    // Shimmer effect
+    if (restaurantData.length === 0) {
+        return (
+            <>
+                <div className="shimmer-container">
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                    <div className="shimmer-wrapper"></div>
+                </div>
+            </>
+        );
+    }
+
     return (
         <>
         <button onClick={() => {
             console.log("Top Rated Restaurants clicked");
-            const filteredData = restaurantData.filter((restaurant) => restaurant.info.avgRating > 4.5);
+            const filteredData = restaurantData.filter((restaurant) => restaurant.info.avgRating >= 4.3);
             setRestaurantData(filteredData);
         }} >Top Rated Restaurants</button>
         <div className="restaurant-container-component">
